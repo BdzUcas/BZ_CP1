7a1e351d4478582760698230858bc527017a8ab5
BdzUcas <bracken.zabriskie@ucas-edu.net>
2025-08-21 09:52:26 -0600

    variables notes and seasons starter

---
+++ notes/variables.py
@@ -0,0 +1,8 @@
+#BZ 1st Variables Notes
+import random
+my_variable = random.random()
+print(my_variable)
+
+my_variable = 6
+print(my_variable)
+
---
+++ starters/seasons.py
@@ -0,0 +1,6 @@
+#BZ 1st sesons
+print("INPUT LAST NAME")
+name = input()
+print("INPUT FAVORITE SEASON")
+season = input()
+print("Wow, " + name + "san, i like " + season + "too!")
