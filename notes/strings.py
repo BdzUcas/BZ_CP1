09ad2a687228d79dc2c28466e49ba4080f6fad07
BdzUcas <bracken.zabriskie@ucas-edu.net>
2025-09-02 09:52:58 -0600

    Strings Notes

---
+++ notes/strings.py
@@ -0,0 +1,52 @@
+# BZ 1st string notes
+
+#i did this in a google doc
+
+wow = """
+ 
+ 
+ hey look it's a multiline string wow
+        It even remembers tabs
+
+
+a
+n
+d
+
+e
+n
+t
+e
+r
+s
+
+ """
+
+
+
+#I nEeD mUlTiLiNe CoMmEnTs'
+
+
+
+
+string = 'The more you know'
+other_string = 'mirror mirror on the wall; who is the fairest of them all?'
+
+name = 'Florian Frederick Fertildirt XIV'
+
+print(name)
+
+number = '5'
+#         ^not a number, that's a string
+
+print(len(other_string))
+print(wow)
+
+
+escape_characters = "\"The Wingfeather Saga\" is the best book series"
+print("Here's string one" + "Here's the second string")
+first_name = input("what's your name? > ")
+full_name = first_name + " Smith"
+print(full_name)
+#indexes
+print(full_name[3:6])
