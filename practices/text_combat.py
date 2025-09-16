4ccab75274520e10f5c7faaae1cdfda7f979fb38
BdzUcas <bracken.zabriskie@ucas-edu.net>
2025-09-16 09:52:52 -0600

    started combat project

---
+++ practices/text_combat.py
@@ -0,0 +1,5 @@
+import random
+first_word = ['Crazy ','Rainbow ','Ultimate ','Incredi','Wimpy ','Awesome ','Interdimensional ','Intimidating ','Forest ','Desert ','Average ','','Zombie ','Robot ','Surprise ','Mystery ']
+second_word = ['Joe','Cthulu','Goblin','Man','Cat','Destroyer','Fred','Pencil','Beholder','Dog','Cultist','Worm','Muppet','Robot','Cupboard','Beast','Hog','Assasin']
+opponent = random.choice(first_word) + random.choice(second_word)
+print(opponent)
