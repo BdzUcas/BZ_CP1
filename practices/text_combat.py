345579aa6448969b2207a81c232300ff986d90ec
BdzUcas <bracken.zabriskie@ucas-edu.net>
2025-09-22 09:53:01 -0600

    started user sign in

--- practices/text_combat.py
+++ practices/text_combat.py
@@ -1,10 +1,9 @@
 import random
 import time
-second_word = ['blablablablabla']
+second_word = ['Joe','Cthulu','Goblin','Man','Cat','Destroyer','Fred','Pencil','Beholder','Dog','Cultist','Worm','Muppet','Robot','Cupboard','Beast','Hog','Assasin','Queen','Idol','Bob','Clang']
 first_word = ['Crazy ','Rainbow ','Ultimate ','Incredi','Wimpy ','Awesome ','Interdimensional ','Intimidating ','Forest ','Desert ','Average ','','Zombie ','Robot ','Surprise ','Mystery ','Jeff the ','Winter ','Sneaky ','Evil ','Demon ','Angel ','Death ','Super']
 miss_message_1 = ['and misses!','but was blinded by their own beauty.','and thinks they hit.','and misses (in a very cool way).','but chickens out.','and misses (in a very cool way).','but accidently teleports away.','but it was just a trick to intimidate you.','but is too slow.','but gets sand in its eyes.','but misses.','but misses.','but their head falls off.','but makes a calculation error.','and almost hits.','but nothing happened...','but misses.','but is too slow.','but it was a trick!','but is too busy cackling maniacally.','but misses.','but their morals caught up to them.','and almost dies trying.',f'but had to stop Evil {random.choice(second_word)} instead.']
 attack_message_1 = ['flails wildly at you','tries to blind you','does some bad karate','does some kung fu','halfheartedly punches at you','does some jujitsu','tries to suck you into a portal','flexs their muscles','grows vines around you','kicks sand at you','punches at you.','attacks you.','bites at you.','charges up a laser','sneak attacks you','does... something','pulls out a weapon','breaths icy air at you','sneak attacks you','tries to use an evil plan','breaths fire at you','starts to smite you','tries to kill you','Tries to stop your evil plans']
-second_word = ['Joe','Cthulu','Goblin','Man','Cat','Destroyer','Fred','Pencil','Beholder','Dog','Cultist','Worm','Muppet','Robot','Cupboard','Beast','Hog','Assasin','Queen','Idol','Bob','Clang']
 miss_message_2 = ['but misses.',"but it doesn't have an effect.",'but was too clumsy.','but misses.','and elegantly misses.','but it hit the ground instead.','but misses','but was just writing a rude word','but gets distracted by its own beauty.','but misses.',"but it doesn't seem to do anything",'but it was innefective',"but it doesn't hit quite right",'but made a calculation error.',"but it doesn't do anything",'but misses','but snorts','but it misses.','but nothing happens.',"but it isn't very impressive",'but sneezed',"But it isn't very effective"]
 attack_message_2 = ['punches at you','chants in an eldritch language','stabs at you','punches at you','pounces at you','tries to obliterate you','punches at you','stabs at you','charges up a beam','bites at you','chants eerily','squirms around','tells a joke','charges up a laser','slams its door','charges at you','charges at you','sneak attacks you','orders her soldiers toward you','calls on thier followers','laughs creepily','Clangs you']
 print('Welcome Challenger!')
@@ -12,6 +11,7 @@ time.sleep(1)
 print('Meet your opponent: ')
 time.sleep(1)
 first_word_choice = random.randint(0,len(first_word)-1)
+first_word_choice = len(first_word)-1
 second_word_choice = random.randint(0,len(second_word)-1)
 opponent = first_word[first_word_choice] + second_word[second_word_choice]
 print(opponent)
---
+++ practices/user_sign_in.py
@@ -0,0 +1,29 @@
+#BZ 1st User Sign In
+users = ['bracken','dirk','warren']
+user_passwords = ['Fred 123','cool_guy7','1443']
+print('Sign In')
+yes_replys = ['y','yes','yep','ye','ya','yeah','yea','yop','sure','absolutly','affirmative','positive','cool with me','yees','yyes','yess','yyeess','yyees','yeess','YEAH','yeah, duh','duh','yup']
+while True:
+    print('\033[34mInput Username:')
+    username = input('> ').lower().strip()
+    if username in users:
+        while True:
+            print('\033[34mInput Password:')
+            password = input('\033[0m> ')
+            if not password in user_passwords:
+                print('\033[31mIncorrect password. Try again:')
+            else:
+                if users.index(username) == user_passwords.index(password):
+                    print(f'\033[32mCorrect Password! Welcome to the program, {username}')
+                    break
+                else:
+                    print('\033[31mIncorrect Password. Try again:')
+    else:
+        print('\033[31mThat account does not exist. Would you like to create it? Y/N:')
+        if input('> ').lower().strip() in yes_replys:
+            print('\033[34mEnter account password:\033[0m')
+            password = input('> ')
+            users.append(username)
+            user_passwords.append(password)
+            print(f'\033[32mAccount created! Welcome to the program, {username}')
+
