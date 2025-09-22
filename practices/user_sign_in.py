516d7ef17d69e4677c0090bb3bf67168211ffe5e
BdzUcas <bracken.zabriskie@ucas-edu.net>
2025-09-22 18:37:11 +0000

    finished user sign in

--- practices/user_sign_in.py
+++ practices/user_sign_in.py
@@ -5,7 +5,7 @@ print('Sign In')
 yes_replys = ['y','yes','yep','ye','ya','yeah','yea','yop','sure','absolutly','affirmative','positive','cool with me','yees','yyes','yess','yyeess','yyees','yeess','YEAH','yeah, duh','duh','yup']
 while True:
     print('\033[34mInput Username:')
-    username = input('> ').lower().strip()
+    username = input('\033[0m> ').lower().strip()
     if username in users:
         while True:
             print('\033[34mInput Password:')
@@ -20,9 +20,9 @@ while True:
                     print('\033[31mIncorrect Password. Try again:')
     else:
         print('\033[31mThat account does not exist. Would you like to create it? Y/N:')
-        if input('> ').lower().strip() in yes_replys:
+        if input('\033[0m> ').lower().strip() in yes_replys:
             print('\033[34mEnter account password:\033[0m')
-            password = input('> ')
+            password = input('\033[0m> ')
             users.append(username)
             user_passwords.append(password)
             print(f'\033[32mAccount created! Welcome to the program, {username}')
