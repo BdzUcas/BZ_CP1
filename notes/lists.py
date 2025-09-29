841da390495eda24fb3c895ccffa4d45da40ca78
BdzUcas <bracken.zabriskie@ucas-edu.net>
2025-09-29 09:45:38 -0600

    lists notes. Also, chaos

---
+++ notes/lists.py
@@ -0,0 +1,34 @@
+#BZ 1st List Notes
+import random as _______
+my_list = ['Deck the halls with bows of holly','Falalalalalalalalala','Tis the season to be jolly','FALALALALALALALALALA']
+my_length = len(my_list)
+my_item = my_list[2]
+
+my_list[2] = 'How much would could a woodchuck chuck if a woodchuck could chuck wood'
+
+weird_list = ['We want us a figgy pudding', 17, False, ['This is a list in a list', True, 954.87], 'HI']
+
+weird_list.append('OOOOOOOOOOOOOOklahoma where the wind comes rushing down the plains')
+
+
+weird_list.pop()
+
+weird_list.remove('We want us a figgy pudding')
+
+weird_list.insert(3, 'BINGLE JELLS BINGLE JELLS WINGLE ALL THE JAAAAAAAYYYYY')
+
+my_list.extend(['Jingle bells','Jingle bells','Jingle all the way','OH WHAT FUN IT IS TO RIDE IN A ONE HORSE OPEN SLAYYYYYYY'])
+
+
+afdslklkjfds = [1, 2, 3, 4, 5, 7, 8, 9]
+afdslklkjfds.clear()
+
+
+#print(my_list, weird_list, afdslklkjfds)
+
+
+
+
+my_tuple = ('this is a tuple','it\'s pretty great','i think','you can\'t change it')
+my_set = {'this is a set','nomnom spinach','INFINITE POWER','sets change their order every time'}
+print(_______.choice(weird_list))
