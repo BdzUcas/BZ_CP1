56418672a6d9f5917ac5e16af22df6e823d62387
BdzUcas <bracken.zabriskie@ucas-edu.net>
2025-10-20 09:53:06 -0600

    functions

---
+++ notes/functions.py
@@ -0,0 +1,13 @@
+#BZ 1st Functions Notes
+
+def camelCase():
+    print("functionsAreNamedInCamelCase")
+camelCase()
+
+
+def a():
+    print('This is a function')
+def b():
+    print('This is b function')
+mylist = range(1,3)
+var = 0
