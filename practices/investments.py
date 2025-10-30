82910fe7e3dfc0d008f57dbaeab78b52c512952a
BdzUcas <bracken.zabriskie@ucas-edu.net>
2025-10-30 09:52:28 -0600

    investments work

--- practices/investments.py
+++ practices/investments.py
@@ -4,12 +4,16 @@ import useful_functions as u
 input_stocks = ['stocks','invest','1','stock','buy','buy stocks','purchase','purchase stocks','invest money']
 input_sell_stocks = ['sell stocks','sell','3','make money']
 input_loan = ['free money','free','2','loan','money','moneys','loans','loan money','bank loan']
-input_end = ['end','terminate','exit','6','done','lose','win','die']
-input_lottery = ['5','lottery']
+input_end = ['end','terminate','exit','7','done','lose','win','die']
+input_lottery = ['6','lottery']
+input_coin_flip = ['4','coin','flip','coin flip']
+input_yes = ['yes','y','yep','yes please','yess','ye','sure','why not','absolutely','let\'s do it','do it','go ahead','coin flip','coin','flip','cool','uh huh','yeah','let\'s start','start','let\'s','lets do it','lets start']
+input_head = ['heads','head','h']
+input_tails = ['tails','tail','t']
 print('Hello.')
 t.sleep(1.5)
 print('Welcome to ....')
-t.sleep(2)
+t.sleep(1.5)
 print('The bank!')
 t.sleep(1)
 print('Yes, the bank.')
@@ -17,11 +21,11 @@ t.sleep(1.5)
 print('Here, we...')
 t.sleep(1)
 print('Invest!')
-t.sleep(1.5)
+t.sleep(1)
 print('There are lots of ways to "invest" your money.')
 t.sleep(1.5)
 print('"Investing" works in turns.')
-t.sleep(1.5)
+t.sleep(1)
 print("Let's start!")
 t.sleep(2)
 turn = 1
@@ -35,6 +39,9 @@ loan_turns = 0
 owed = 0
 lottery = 0
 lottery_value = 10000
+coin = 0
+coinchoice = 0
+betting_value = 5
 while True:
     print(f'Turn {turn}. You have ${money}, and {stocks} stocks.')
     t.sleep(1)
@@ -75,7 +82,7 @@ while True:
             print('You didn\'t win. Too bad!')
         lottery = 0
     t.sleep(1)
-    activity = input('1. Stocks \n2. "Free money"\n3. Sell Stocks \n4. Pass\n5. Lottery\n6. End\n').lower().strip()
+    activity = input('1. Stocks \n2. "Free money"\n3. Sell Stocks \n4. Coin Flip\n5. Pass\n6. Lottery\n7. End\n').lower().strip()
     if activity in input_stocks:
         print('Welcome to the stock market!')
         t.sleep(2)
@@ -116,7 +123,7 @@ while True:
             print('You already have money from me!')
             t.sleep(1)
             continue
-        print('You need some money, ay?')
+        print('You need some money, egh?')
         t.sleep(2)
         print('While don\'t worry about it! I got you covered.')
         t.sleep(2)
@@ -127,6 +134,8 @@ while True:
         loan = u.int_input('So, what\'ll it be?\n','That\'s not a number, now is it?')
         if loan == 0:
             print('No money? Okay then.')
+        elif loan > 10000:
+            print('No that\'s a bit ridiculous, don\'t you think?')
         else:
             print(f'Here\'s your ${loan}. Spend it wisely! I\'ll come collect the return in 5 turns.')
             money += loan
@@ -138,7 +147,9 @@ while True:
         score = money - 100
         print(f'Thanks for investing! Your score was {score}.')
         if score < 0:
-            print("You lost money! That's sad. Sad for you.")
+            print("You lost money! That's sad.")
+            t.sleep(1)
+            print('Sad for you.')
         elif score == 0:
             print("You broke even! Try harder next time.")
         elif score > 0 and score <= 100:
@@ -165,8 +176,62 @@ while True:
             lottery += tickets
             lottery_value += tickets
             print('Thank you for buying tickets!')
-    elif activity == 'its me':
-        money += 10000
+    elif activity in input_coin_flip:
+        print('Welcome to the coin flip!')
+        t.sleep(1)
+        print('Here you can play a fun game of flipping coins!')
+        t.sleep(1.5)
+        print(f'And you choose the price!')
+        t.sleep(1)
+        print('Well then, shall we?')
+        yes_or_no = input().replace('!','').strip().lower()
+        if yes_or_no in input_yes:
+            betting_value = u.int_input('How much would you like to wager?\n','That\'s not a number, now is it?')
+            if money >= betting_value:
+                money -= betting_value
+                t.sleep(0.5)
+                print('Heads or tails?')
+                while not False:
+                    heads_or_tails = input().strip().lower()
+                    if heads_or_tails in input_head:
+                        coinchoice = 1
+                        break
+                    elif heads_or_tails in input_tails:
+                        coinchoice = 0
+                        break
+                    elif heads_or_tails == 'either':
+                        coinchoice = r.randint(0,1)
+                        break
+                    else:
+                        print('Please pick heads or tails!')
+                if coinchoice == 0:
+                    print('You are tails!')
+                elif coinchoice == 1:
+                    print('You are heads!')
+                t.sleep(1)
+                betting_value *= 2
+                print(f'There is ${betting_value} on the table!')
+                t.sleep(1)
+                print('Flipping coin...')
+                t.sleep(1)
+                coin = r.randint(0,1)
+                if coin == 0:
+                    print('The coin is tails!')
+                elif coin == 1:
+                    print('The coin is heads!')
+                t.sleep(1)
+                if coin == coinchoice:
+                    print(f'You win ${betting_value}!')
+                    money += betting_value
+                    t.sleep(1)
+                    print('Come back later, will you?')
+                else:
+                    print('Aw, you lost.')
+                    t.sleep(1)
+                    print('Too bad!')
+                    t.sleep(1)
+            else:
+                print('You don\'t have enough money!')
     else:
         print('You passed this turn.')
     t.sleep(2)
