e4e47ebd66dc7e4b3a086e32dacd4a181c4b4eaa
BdzUcas <bracken.zabriskie@ucas-edu.net>
2025-10-31 18:46:08 +0000

    maze generator work 1

--- practices/maze_generator.py
+++ practices/maze_generator.py
@@ -11,14 +11,14 @@ for y in range(0,size):
     row = []
     #loop through x coordinates of the maze
     for x in range(0,size):
-        #add "1111" to row list. This represents the sides, with 1 being wall and 0 being no wall, starting at the top and moving clockwise
-        row.append('1111')
+        #add [1,1,1,1] to row list. This represents the sides, with 1 being wall and 0 being no wall, starting at the top and moving clockwise
+        row.append([1,1,1,1])
     #add row list to maze list
     maze.append(row)
 #pick a random x coordinate from the maze
-x = r.randint(1,size)
+x = r.randint(0,size-1)
 #pick a random y coordinate from the maze
-y = r.randint(1,size)
+y = r.randint(0,size-1)
 #create an empty "visited" list
 visited = []
 #create an empty "stack" list
@@ -29,8 +29,34 @@ while len(visited) < size ** 2:
     visited.append([x,y])
     stack.append([x,y])
     #create a list of all neighbor cells
-    #remove items that match an item in the visited list
+    neighbors = [[x,y+1],[x-1,y],[x+1,y],[x,y-1]]
+    #remove items that match an item in the visited list OR contain coords outside of the maze
+    for i in neighbors:
+        if i in visited:
+            neighbors.remove(i)
+        elif -1 in i:
+            neighbors.remove(i)
+        elif size in i:
+            neighbors.remove(i)
     #pick a random item from the neighbors list
-    #remove wall between current cell and chosen neighbor cell
-    #set x and y coordinate to that of the chosen neighbor
+    neighbor = r.choice(neighbors)
     
+    #remove wall between current cell and chosen neighbor cell
+    if neighbor[0] == x - 1:
+        neighbor_remove = 1
+        self_remove = 3
+    elif neighbor[0] == x+1:
+        neighbor_remove = 3
+        self_remove = 1
+    elif neighbor[0] == x:
+        if neighbor[1] == y-1:
+            neighbor_remove = 0
+            self_remove = 2
+        elif neighbor[1] == y+1:
+            neighbor_remove = 2
+            self_remove = 0
+    maze[y][x][neighbor_remove] = 0
+    maze[y][x][self_remove] = 0
+    #set x and y coordinate to that of the chosen neighbor
+    x,y = neighbor
+print(visited)
